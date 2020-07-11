ed2dbd2ed8316c98868c7719dccc977678790eca
jieru <50067815+yuanyuan999@users.noreply.github.com>
2020-07-11 20:09:38 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ MONGO_TABLE = 'product'
 
 SERVICE_ARGS = ['--load-images=false','--disk-cache=true']
 
-username = '15686785148'
-password = 'yuanpeng19990919'
-KEYWORD = '美食'
+username = '输入你的淘宝账号'
+password = '输入你的淘宝密码'
+KEYWORD = '写你想搜的内容'
